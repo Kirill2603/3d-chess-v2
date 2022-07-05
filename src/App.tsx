18f235ebadf215892c1ef0e108a95194b585d4ff
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-07-05 11:34:13 +0300

    Fix models

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
-import { Castle } from './components/figures/Castle'
-import { King } from './components/figures/King'
-import { Knight } from './components/figures/Knight'
-import { Pawn } from './components/figures/Pawn'
-import { Queen } from './components/figures/Queen'
-import { Rook } from './components/figures/Rook'
 import { Board } from './components/Board'
 import { useAppSelector } from './store/store'
 
@@ -26,12 +20,6 @@ function App() {
       <Environment preset='apartment' background />
       <Suspense fallback={null}>
         <Environment preset='dawn' background />
-        {/*<Castle />*/}
-        {/*<King />*/}
-        {/*<Knight />*/}
-        {/*<Pawn />*/}
-        {/*<Queen />*/}
-        {/*<Rook />*/}
         {figures.map(({ Figure, id, color, position }) => {
           return (
             <Figure key={id} id={id} color={color} position={position} />
