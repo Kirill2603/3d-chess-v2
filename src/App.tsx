1f89de013538ff00c70604e869de06bb9fea8a63
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-07-05 18:46:10 +0300

    add castle and rook moves

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
 import { Board } from './components/Board/Board'
