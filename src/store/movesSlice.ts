6ac81620332c777226ce5e6d6d8bb2ed24cb646a
Kirill <64753246+Kirill2603@users.noreply.github.com>
2022-07-06 19:08:10 +0300

    change movesSlice.ts

--- src/store/movesSlice.ts
+++ src/store/movesSlice.ts
@@ -8,13 +8,106 @@ const initialState: AvailableMovesType = {
   availableMoves: [],
 }
 
+const filterMoves = (moves: Array<[number, number, number]>): Array<[number, number, number]> => {
+  return moves.filter((move) => {
+    if ((move[0] < 0 || move[2] < 0) || (move[0] >= 8 || move[2] >= 8)) {
+      return false
+    } else {
+      return move
+    }
+  })
+}
+
+const knightMovePattern = (position: [number, number, number]): Array<[number, number, number]> => {
+  const [x, y, z] = position
+  const moves: Array<[number, number, number]> = []
+  moves.push(
+    [x + 2, y, z + 1],
+    [x + 1, y, z + 2],
+    [x - 1, y, z - 2],
+    [x - 2, y, z - 1],
+    [x - 2, y, z + 1],
+    [x + 1, y, z - 2],
+    [x - 1, y, z + 2],
+    [x + 2, y, z - 1],
+  )
+  return filterMoves(moves)
+}
+
+const queenMovesPattern = (position: [number, number, number]): Array<[number, number, number]> => {
+  const [x, y, z] = position
+  const moves: Array<[number, number, number]> = []
+  let i = 1
+  while (i < 8) {
+    moves.push(
+      [x + i, y, z + i],
+      [x - i, y, z - i],
+      [x + i, y, z - i],
+      [x - i, y, z + i],
+      [x, y, z + i],
+      [x, y, z - i],
+      [x + i, y, z],
+      [x - i, y, z],
+    )
+    i++
+  }
+  return filterMoves(moves)
+}
+
+const castleMovesPattern = (position: [number, number, number]): Array<[number, number, number]> => {
+  const [x, y, z] = position
+  const moves: Array<[number, number, number]> = []
+  let i = 1
+  while (i < 8) {
+    moves.push(
+      [x, y, z + i],
+      [x, y, z - i],
+      [x + i, y, z],
+      [x - i, y, z],
+    )
+    i++
+  }
+  return filterMoves(moves)
+}
+
+const rookMovesPattern = (position: [number, number, number]): Array<[number, number, number]> => {
+  const [x, y, z] = position
+  const moves: Array<[number, number, number]> = []
+  let i = 1
+  while (i < 8) {
+    moves.push(
+      [x + i, y, z + i],
+      [x - i, y, z - i],
+      [x + i, y, z - i],
+      [x - i, y, z + i])
+    i++
+  }
+  return filterMoves(moves)
+}
+
+const kingMovePattern = (position: [number, number, number]): Array<[number, number, number]> => {
+  const [x, y, z] = position
+  const moves: Array<[number, number, number]> = []
+  moves.push(
+    [x + 1, y, z - 1],
+    [x + 1, y, z],
+    [x + 1, y, z + 1],
+    [x, y, z + 1],
+    [x, y, z - 1],
+    [x - 1, y, z + 1],
+    [x - 1, y, z],
+    [x - 1, y, z - 1],
+  )
+  return filterMoves(moves)
+}
+
 export const movesSlice = createSlice({
   name: 'availableMoves',
   initialState,
   reducers: {
     getAvailableMoves: (state, action: PayloadAction<{ id: string | null, position: [number, number, number] }>) => {
       const [x, y, z] = action.payload.position
-      console.log(action.payload.id)
+
       if (action.payload.id && action.payload.id.includes('Pawn')) {
         if (action.payload.id[0] === 'W') {
           if (x === 1) {
@@ -31,110 +124,32 @@ export const movesSlice = createSlice({
         }
       }
 
-      if (action.payload.id && action.payload.id.includes('Castle')) {
-        let i = 1
-        while (i !== 8) {
-          if (!((x + i) >= 8)) {
-            state.availableMoves.push([x + i, y, z])
-          }
-          if (!((z + i) >= 8)) {
-            state.availableMoves.push([x, y, z + i])
-          }
-          if (!((x - i) < 0)) {
-            state.availableMoves.push([x - i, y, z])
-          }
-          if (!((z - i) < 0)) {
-            state.availableMoves.push([x, y, z - i])
-          }
-          i++
-        }
+      if (action.payload.id && action.payload.id.includes('King')) {
+        const availableMoves = kingMovePattern(action.payload.position)
+        state.availableMoves.push(...availableMoves)
       }
 
-      // if (action.payload.id.includes('King')) {
-      //   console.log('King', 'position:', action.payload.position, 'color', action.payload.id[0])
-      // }
-      //
       if (action.payload.id && action.payload.id.includes('Rook')) {
-        let i = 1
-        while ((x + i) < 8 && (z + i) < 8) {
-          state.availableMoves.push([x + i, y, z + i])
-          i++
-        }
-        i = 1
-        while ((x - i) >= 0 && (z - i) >= 0) {
-          state.availableMoves.push([x - i, y, z - i])
-          i++
-        }
-        i = 1
-        while ((x + i) < 8 && (z - i) >= 0) {
-          state.availableMoves.push([x + i, y, z - i])
-          i++
-        }
-        while ((x - i) >= 0 && (z + i) < 8) {
-          state.availableMoves.push([x - i, y, z + i])
-          i++
-        }
+        const availableMoves = rookMovesPattern(action.payload.position)
+        state.availableMoves.push(...availableMoves)
       }
-      //
+
+      if (action.payload.id && action.payload.id.includes('Castle')) {
+        const availableMoves = castleMovesPattern(action.payload.position)
+        state.availableMoves.push(...availableMoves)
+      }
+
       if (action.payload.id && action.payload.id.includes('Queen')) {
-        let i = 1
-        while (i !== 8) {
-          if (!((x + i) >= 8)) {
-            state.availableMoves.push([x + i, y, z])
-          }
-          if (!((z + i) >= 8)) {
-            state.availableMoves.push([x, y, z + i])
-          }
-          if (!((x - i) < 0)) {
-            state.availableMoves.push([x - i, y, z])
-          }
-          if (!((z - i) < 0)) {
-            state.availableMoves.push([x, y, z - i])
-          }
-          i++
-        }
-        i = 1
-        while ((x + i) < 8 && (z + i) < 8) {
-          state.availableMoves.push([x + i, y, z + i])
-          i++
-        }
-        i = 1
-        while ((x - i) >= 0 && (z - i) >= 0) {
-          state.availableMoves.push([x - i, y, z - i])
-          i++
-        }
-        i = 1
-        while ((x + i) < 8 && (z - i) >= 0) {
-          state.availableMoves.push([x + i, y, z - i])
-          i++
-        }
-        while ((x - i) >= 0 && (z + i) < 8) {
-          state.availableMoves.push([x - i, y, z + i])
-          i++
-        }
+        const availableMoves = queenMovesPattern(action.payload.position)
+        state.availableMoves.push(...availableMoves)
       }
 
       if (action.payload.id && action.payload.id.includes('Knight')) {
-        const moves: Array<[number, number, number]> = [
-          [x + 2, y, z + 1],
-          [x + 1, y, z + 2],
-          [x - 1, y, z - 2],
-          [x - 2, y, z - 1],
-          [x - 2, y, z + 1],
-          [x + 1, y, z - 2],
-          [x - 1, y, z + 2],
-          [x + 2, y, z - 1]]
-
-        const filteredMoves = moves.filter((arr) => {
-          if (!((arr[0] < 0 || arr[2] < 0) || (arr[0] >= 8 || arr[2] >= 8))) {
-            return arr
-          } else {
-            return false
-          }
-        })
-
-        state.availableMoves.push(...filteredMoves)
+        const availableMoves = knightMovePattern(action.payload.position)
+        state.availableMoves.push(...availableMoves)
       }
+
+
     },
     resetMoves: (state) => {
       state.availableMoves = []
