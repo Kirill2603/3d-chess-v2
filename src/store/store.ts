1b54fa3c46421052ce040c00c9ad9b186d9c8fd3
kirill2603 <k.radobolsky@gmail.com>
2022-07-12 17:27:57 +0300

    start  refactor

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { boardSlice } from './boardSlice'
-import { figuresSlice } from './figuresSlice'
-import { selectedSlice } from './selectedSlice'
 
 export const store = configureStore({
   reducer: {
-    board: boardSlice.reducer,
-    figures: figuresSlice.reducer,
-    selectedFigure: selectedSlice.reducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+
+  }
 });
 
 export type AppDispatch = typeof store.dispatch
