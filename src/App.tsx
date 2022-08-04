b836e197263de824bc72a17089eb02c734d04eee
kirill2603 <k.radobolsky@gmail.com>
2022-08-04 17:30:04 +0300

    dev test (try chakraUI)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
 import Scene from './components/SceneComponents/Scene'
-import Sidebar from './components/Sidebar/Sidebar'
-import styles from './app.module.css'
-import PlayerStats from './components/PlayerStats'
-import { useAppSelector } from './store/store'
 
 function App() {
 
-  const { history, isCheck, isMate, player } = useAppSelector(state => state.game)
+  // const { history, isCheck, isMate, player } = useAppSelector(state => state.game)
 
   return (
-    <div className={styles.app}>
-      <div className={styles.appBlock}>
-        <PlayerStats player='b' isCheck={isCheck} isMate={isMate} activePlayer={player} history={history}/>
-        <Scene />
-        <PlayerStats player='w' isCheck={isCheck} isMate={isMate} activePlayer={player} history={history}/>
-      </div>
-      <Sidebar history={history}/>
-    </div>
-
+    <Scene />
   )
 }
 
